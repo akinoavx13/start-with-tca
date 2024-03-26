1a75ebc51e31bfb766559faabe8210375e270415
Maxime Maheo <imaxame@gmail.com>
2024-03-26 15:50:47 +0100

    Navigation

---
+++ StartWithTCA/AddContactFeature.swift
@@ -0,0 +1,89 @@
+//
+//  AddContactFeature.swift
+//  StartWithTCA
+//
+//  Created by Maxime Maheo on 26/03/2024.
+//
+
+import ComposableArchitecture
+import SwiftUI
+
+@Reducer
+struct AddContactFeature {
+    @ObservableState
+    struct State: Equatable {
+        var contact: Contact
+    }
+
+    enum Action {
+        case cancelButtonTapped
+        case saveButtonTapped
+        case setName(String)
+        case delegate(Delegate)
+
+        enum Delegate: Equatable {
+            case saveContact(Contact)
+        }
+    }
+
+    @Dependency(\.dismiss) var dismiss
+
+    var body: some ReducerOf<Self> {
+        Reduce { state, action in
+            switch action {
+            case .cancelButtonTapped:
+                return .run { _ in
+                    await dismiss()
+                }
+
+            case .saveButtonTapped:
+                return .run { [contact = state.contact] send in
+                    await send(.delegate(.saveContact(contact)))
+                    await dismiss()
+                }
+
+            case let .setName(name):
+                state.contact.name = name
+                return .none
+
+            case .delegate:
+                return .none
+            }
+        }
+    }
+}
+
+struct AddContactView: View {
+    @Bindable var store: StoreOf<AddContactFeature>
+
+    var body: some View {
+        Form {
+            TextField("Name", text: $store.contact.name.sending(\.setName))
+
+            Button("Save") {
+                store.send(.saveButtonTapped)
+            }
+        }
+        .toolbar {
+            ToolbarItem {
+                Button("Cancel") {
+                    store.send(.cancelButtonTapped)
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    NavigationStack {
+        AddContactView(
+            store: Store(
+                initialState: AddContactFeature.State(
+                    contact: Contact(id: UUID(), name: "Blob")
+                )
+            ) {
+                AddContactFeature()
+            }
+        )
+    }
+}
---
+++ StartWithTCA/ContactsFeature.swift
@@ -0,0 +1,94 @@
+//
+//  ContactsFeature.swift
+//  StartWithTCA
+//
+//  Created by Maxime Maheo on 26/03/2024.
+//
+
+import ComposableArchitecture
+import Foundation
+import SwiftUI
+
+struct Contact: Equatable, Identifiable {
+    let id: UUID
+    var name: String
+}
+
+@Reducer
+struct ContactsFeature {
+    @ObservableState
+    struct State: Equatable {
+        @Presents var addContact: AddContactFeature.State?
+        var contacts: IdentifiedArrayOf<Contact> = []
+    }
+
+    enum Action {
+        case addButtonTapped
+        case addContact(PresentationAction<AddContactFeature.Action>)
+    }
+
+    var body: some ReducerOf<Self> {
+        Reduce { state, action in
+            switch action {
+            case .addButtonTapped:
+                state.addContact = AddContactFeature.State(contact: Contact(id: UUID(), name: ""))
+                return .none
+
+            case let .addContact(.presented(.delegate(.saveContact(contact)))):
+                state.contacts.append(contact)
+                return .none
+
+            case .addContact:
+                return .none
+            }
+        }
+        .ifLet(\.$addContact, action: \.addContact) {
+            AddContactFeature()
+        }
+    }
+}
+
+struct ContactsView: View {
+    @Bindable var store: StoreOf<ContactsFeature>
+
+    var body: some View {
+        NavigationStack {
+            List {
+                ForEach(store.contacts) { contact in
+                    Text(contact.name)
+                }
+            }
+            .navigationTitle("Contacts")
+            .toolbar {
+                ToolbarItem {
+                    Button {
+                        store.send(.addButtonTapped)
+                    } label: {
+                        Image(systemName: "plus")
+                    }
+                }
+            }
+        }
+        .sheet(item: $store.scope(state: \.addContact, action: \.addContact)) { store in
+            NavigationStack {
+                AddContactView(store: store)
+            }
+        }
+    }
+}
+
+#Preview {
+    ContactsView(
+        store: Store(
+            initialState: ContactsFeature.State(
+                contacts: [
+                    Contact(id: UUID(), name: "Blob"),
+                    Contact(id: UUID(), name: "Blob Jr"),
+                    Contact(id: UUID(), name: "Blob Sr")
+                ]
+            )
+        ) {
+            ContactsFeature()
+        }
+    )
+}
